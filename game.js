307d9335e7c9f6423abdca23cdeee83882b69956
rniemela77 <rvniemela@hotmail.com>
2025-02-01 13:17:55 -0700

    game working

--- game.js
+++ game.js
@@ -1,39 +1,49 @@
+class MyScene extends Phaser.Scene {
+    constructor() {
+        super({ key: 'MyScene' });
+    }
+
+    preload() {
+        // Load assets here
+    }
+
+    create() {
+        console.log('Create function is called');
+        this.graphics = this.add.graphics({ lineStyle: { width: 4, color: 0x00ff00 } });
+        this.curve = new Phaser.Curves.QuadraticBezier(new Phaser.Math.Vector2(100, 300), new Phaser.Math.Vector2(400, 500), new Phaser.Math.Vector2(700, 300));
+        this.ball = this.add.circle(400, 0, 10, 0xff0000);
+        this.physics.add.existing(this.ball);
+        this.ball.body.setGravityY(300);
+
+        this.input.on('pointermove', (pointer) => {
+            const angle = Phaser.Math.Linear(0, Math.PI / 4, pointer.x / this.sys.game.config.width);
+            this.curve.p0.set(100, 300);
+            this.curve.p1.set(400, 500 + Math.sin(angle) * 100);
+            this.curve.p2.set(700, 300);
+        });
+    }
+
+    update() {
+        this.graphics.clear();
+        const points = this.curve.getPoints(32);
+        for (let i = 1; i < points.length; i++) {
+            this.graphics.lineBetween(points[i-1].x, points[i-1].y, points[i].x, points[i].y);
+        }
+    }
+}
+
 const config = {
     type: Phaser.AUTO,
-    width: 800,
-    height: 600,
-    scene: {
-        preload: preload,
-        create: create,
-        update: update
-    }
+    width: window.innerWidth,
+    height: window.innerHeight,
+    physics: {
+        default: 'arcade',
+        arcade: {
+            gravity: { y: 0 },
+            debug: false
+        }
+    },
+    scene: MyScene
 };
 
 const game = new Phaser.Game(config);
-
-function preload() {
-    // Load assets here
-}
-
-function create() {
-    // Initialize game objects here
-    this.graphics = this.add.graphics({ lineStyle: { width: 4, color: 0x00ff00 } });
-    this.curve = new Phaser.Curves.QuadraticBezier(new Phaser.Math.Vector2(100, 300), new Phaser.Math.Vector2(400, 500), new Phaser.Math.Vector2(700, 300));
-    this.ball = this.add.circle(400, 0, 10, 0xff0000);
-    this.physics.add.existing(this.ball);
-    this.ball.body.setGravityY(300);
-
-    this.input.on('pointermove', (pointer) => {
-        const angle = Phaser.Math.Linear(0, Math.PI / 4, pointer.x / this.sys.game.config.width);
-        this.curve.p0.set(100, 300);
-        this.curve.p1.set(400, 500 + Math.sin(angle) * 100);
-        this.curve.p2.set(700, 300);
-    });
-}
-
-function update() {
-    // Clear previous graphics
-    this.graphics.clear();
-    // Draw the curve
-    this.graphics.strokeCurve(this.curve);
-}
